3815d9c0a54978a0ff178079c17042a56d16ceaf
rm344474 <rm344474@fiap.com.br>
2022-09-10 18:40:35 -0300

    Health Check Controller

--- src/main/java/br/com/fiap/abctechapi/controller/HeauthCheckController.java
+++ src/main/java/br/com/fiap/abctechapi/controller/HeauthCheckController.java
@@ -5,12 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 @RestController //Informa que é um controller
 @RequestMapping("/")//Mapeamento pela raiz
 public class HeauthCheckController {
 
     @GetMapping("version")
-    public ResponseEntity<Object> status(){
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Object> status() throws Exception{
+        Properties properties = new Properties();//Pega as propriedades
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.yaml");//leitura de propriedades
+        properties.load(inputStream);
+        return ResponseEntity.ok(properties.getProperty("build.name") + " - " + properties.getProperty("build.version"));
     }
 }
